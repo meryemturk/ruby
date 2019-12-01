fddfa88e2a0b02fe445a4f3e3d33e1412de23e1d
meryemturk <meryem12turk@gmail.com>
2019-12-01 16:53:33 +0800

    exercise two done

---
+++ exercise_2.rb
@@ -0,0 +1,13 @@
+def calculator
+    puts "give me the first number"
+    first_number = gets.chomp.to_i
+    puts "give me the second number"
+    second_number = gets.chomp.to_i
+    puts "great job! the two numbers added up is #{first_number+second_number}"
+    puts "#{first_number}-#{second_number} will be #{first_number-second_number}"
+    puts "#{first_number} * #{second_number} will be #{first_number*second_number}"
+    puts "lastly #{first_number} devided by #{second_number} is #{first_number/second_number}"
+
+end
+calculator()
+
