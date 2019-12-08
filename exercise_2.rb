9efe17636490a7596a1708dfdd7233999c2df8bd
meryemturk <meryem12turk@gmail.com>
2019-12-08 12:47:08 +0800

    exerise sixteen done

--- exercise_2.rb
+++ exercise_2.rb
@@ -1,13 +1,12 @@
 def calculator
-    puts "give me the first number"
-    first_number = gets.chomp.to_i
-    puts "give me the second number"
-    second_number = gets.chomp.to_i
-    puts "great job! the two numbers added up is #{first_number+second_number}"
-    puts "#{first_number}-#{second_number} will be #{first_number-second_number}"
-    puts "#{first_number} * #{second_number} will be #{first_number*second_number}"
-    puts "lastly #{first_number} devided by #{second_number} is #{first_number/second_number}"
-
+  puts "give me the first number"
+  first_number = gets.chomp.to_i
+  puts "give me the second number"
+  second_number = gets.chomp.to_i
+  puts "great job! the two numbers added up is #{first_number + second_number}"
+  puts "#{first_number}-#{second_number} will be #{first_number - second_number}"
+  puts "#{first_number} * #{second_number} will be #{first_number * second_number}"
+  puts "lastly #{first_number} devided by #{second_number} is #{first_number / second_number}"
 end
-calculator()
 
+calculator()
--- exercise_9.rb
+++ exercise_9.rb
@@ -1,12 +1,13 @@
 def calculator_3
-    puts "give me the fist number?"
-    first_number = gets.chomp.to_i
-    puts "give me the second number?"
-    second_number = gets.chomp.to_i
-    puts "#{first_number}+#{second_number}=#{first_number+second_number}"
-    puts "#{first_number}-#{second_number}=#{first_number-second_number}"
-    puts "#{first_number}*#{second_number}=#{first_number*second_number}"
-    puts "#{first_number} divided by #{second_number} is equal to #{first_number/second_number} with a balance of #{first_number-second_number}"        
-    puts "The number in ones place is #{first_number}"
-end 
+  puts "give me the fist number?"
+  first_number = gets.chomp.to_i
+  puts "give me the second number?"
+  second_number = gets.chomp.to_i
+  puts "#{first_number}+#{second_number}=#{first_number + second_number}"
+  puts "#{first_number}-#{second_number}=#{first_number - second_number}"
+  puts "#{first_number}*#{second_number}=#{first_number * second_number}"
+  puts "#{first_number} divided by #{second_number} is equal to #{first_number / second_number} with a balance of #{first_number - second_number}"
+  puts "The number in ones place is #{first_number}"
+end
+
 calculator_3()
