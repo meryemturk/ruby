850b32a6d24c3280d9cffc1666e9a3cae4948d08
meryemturk <meryem12turk@gmail.com>
2019-12-08 14:34:08 +0800

    exercise four done

--- exercise_4.rb
+++ exercise_4.rb
@@ -1,9 +1,10 @@
 def greeter
-    puts "what is the reversed of your full name?"
-    full_name = gets.chomp
-    puts "how many characters does your name has?"
-    characters = gets.chomp.to_i
-    puts "how do you write your name in uppercase?"
-    uppercase = gets.chomp    
+  puts "what is your name?"
+  full_name = gets.chomp
+  puts "your full name reversed is #{full_name.reverse}?"
+  puts "your full name has #{full_name.length} characters"
+  puts "your full name uppercase is #{full_name.upcase}"
+  uppercase = gets.chomp
 end
+
 greeter()
