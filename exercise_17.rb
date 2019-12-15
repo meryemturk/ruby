0b17f832ec80163ce2334fae51cc6f54a8b25a6d
meryemturk <meryem12turk@gmail.com>
2019-12-15 17:45:03 +0800

    exercise seventeen done

--- exercise_17.rb
+++ exercise_17.rb
@@ -1,8 +1,11 @@
-def reversing_an_array
-    for number in (1..10)
-    
-    end
-   
-        
-end 
-reversing_an_array()
+def reversing_an_array(array)
+  array1 = []
+  array.length.times do |x|
+    a = array.pop
+    array1.push(a)
+  end
+  return array1
+end
+
+a = [1, 2, 3, 4, 5, 6]
+p reversing_an_array(a)
